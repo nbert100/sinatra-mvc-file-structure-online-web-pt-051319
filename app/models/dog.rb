002a4b948808223bb74e2b45329d4341e789bc34
Nia Bert <nbert14@gmail.com>
2019-08-12 01:43:56 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+    attr_reader :breed
+    attr_accessor :name, :age
+
+    @@dogs = []
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age 
+        @@dogs << self
+    end
+
+    def self.all 
+        @@dogs
+    end
+end
