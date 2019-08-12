cb3288c98a292d3dd75d39715c107cdeffef54c2
Nia Bert <nbert14@gmail.com>
2019-08-12 01:43:22 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,17 @@
 class Dog 
-  attr_accessor :name, :breed, :age 
-  
-  @@all = []
-  
-  def initialize(name, breed, age)
-    @name = name 
-    @breed = breed
-    @age = age
-    @@all << self
-  end
-  
-  def all
-    @@all
-  end
+    attr_reader :breed
+    attr_accessor :name, :age
+
+    @@dogs = []
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age 
+        @@dogs << self
+    end
+
+    def self.all 
+        @@dogs
+    end
 end
